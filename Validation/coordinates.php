b20d27afec49182baaa3700e8416c8f26620c933
KhurshedSaidov <asgfhsjsks@gmail.com>
2022-01-30 19:23:43 +0500

    add coordinates

---
+++ Validation/coordinates.php
@@ -0,0 +1,48 @@
+<?php
+
+function typeChecking($latutude, $longtitude)
+{
+    var_dump(is_float($latutude));
+    var_dump(is_float($longtitude));
+}
+
+function vallenght(int $latutude, int $longtitude)
+{
+    echo strlen($latutude); echo "<br>";
+    echo strlen($longtitude); echo "<br>";
+}
+
+function mysquare(int $latutude, int $longtitude)
+{
+    $min = 8;
+    $max = 15;
+    if (strlen(string: $latutude)  >= $max)
+{
+    return "A lot of characters in coordinates. ";
+}
+    if (strlen(string: $longtitude)  >= $max)
+{
+    return "A lot of characters in coordinates. ";
+}
+    if (strlen($latutude, $latutude) < $min)
+{
+    return "Few characters in coordinates. ";
+}
+    if (strlen($latutude, $longtitude) < $min)
+{
+    return "Few characters in coordinates. ";
+}
+
+function coordinates(int $latutude, int $longtitude)
+{
+    $latutude       = ["23134.12331"];
+    $longtitude     = ["2313213.4244"];
+    $type_checking  = typeChecking($latutude, $longtitude);
+    $val_lenght     = vallenght($latutude, $longtitude);
+    $in_my_square   = mysquare($latutude, $longtitude);
+    return "$type_checking $val_lenght $in_my_square";
+
+}
+$latutude   = readline("Input latutude: ");
+$longtitude = readline("Input longtitude: ");
+echo coordinates($latutude, $longtitude);
