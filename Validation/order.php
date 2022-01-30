629ac4f63ff58503eb6c7473c290b232fa3163c5
Mohammad <tajweb.m@gmail.com>
2022-01-31 01:23:26 +0500

    edited Functions names and max-min, added is_numeric

--- Validation/order.php
+++ Validation/order.php
@@ -1,38 +1,39 @@
 <?php
 // Проверить типы данных
-function validateTypeOfFishName ($fish_name){
+function validateString($fish_name)
+{
     if ((int)$fish_name != 0) {
         return "422: The type of Fish name is incorrectly! ";
-    }
-    else return "200: Fish name type is ok! ";
+    } else return "200: Fish name type is ok! ";
 }
 
-function validateTypeOfFishNumber ($fish_number){
-    if ((int)$fish_number == 0) {
-        return "422: The type of Fish number is incorrectly! ";
-    }
-    else return "200: Fish number type is ok! ";
+function validateInteger($fish_number)
+{
+    if (is_numeric($fish_number)) {
+        return "200: Fish number type is ok! ";
+    } else return "422: The type of Fish number is incorrectly! ";
+
 }
 
 // Длина команды минимум и максимум
-function validateMin ($fish_name){
-    $min = 3;
-    if (strlen($fish_name) < $min) {
+function validateMin($fish_name, $min_value)
+{
+    if (strlen($fish_name) <= $min_value) {
         return "Very few characters in the order! ";
-    }
-    else return "Min number of characters are ok! ";
+    } else return "Min number of characters are ok! ";
 }
 
-function validateMax ($fish_name){
-    $max = 12;
-    if (strlen($fish_name) > $max) {
+function validateMax($fish_name, $max_value)
+{
+    if (strlen($fish_name) >= $max_value) {
         return "A lot of characters in the order! ";
-    }
-    else return "Max number of characters are ok! ";
+
+    } else return "Max number of characters are ok! ";
 }
 
 // находится для в наборе уже существующих рыб в океане
-function isInArray ($fish_name) {
+function validateInArray($fish_name)
+{
     $fishes = array(
         'Щука',
         'Судак',
@@ -43,22 +44,24 @@ function isInArray ($fish_name) {
     );
     if (in_array($fish_name, $fishes)) {
         return "The fish is in list!";
-    }
-    else return "The fish is not in list!";
+    } else return "The fish is not in list!";
 }
 
-function FishermanOrder($fish_name, $fish_number)
+function orderToFisherman($fish_name, $fish_number, $min_value, $max_value)
 {
-    $str_validate_type = validateTypeOfFishName($fish_name);
-    $int_validate_type = validateTypeOfFishNumber($fish_number);
-    $validate_min = validateMin($fish_name);
-    $validate_max = validateMax($fish_name);
-    $in_array = isInArray($fish_name);
-    return $str_validate_type . $int_validate_type . $validate_min . $validate_max . $in_array;
+    $validate_string = validateString($fish_name);
+    $validate_integer = validateInteger($fish_number);
+    $validate_min = validateMin($fish_name, $min_value);
+    $validate_max = validateMax($fish_name, $max_value);
+    $in_array = validateInArray($fish_name);
+    return $validate_string . $validate_integer . $validate_min . $validate_max . $in_array;
 }
 
 
 //business logic
 $fish_name = readline("Input the name of fish, please: ");
 $fish_number = readline("Input the number of fish, please: ");
-echo FishermanOrder($fish_name, $fish_number);
+$min_value = 3;
+$max_value = 20;
+
+echo orderToFisherman($fish_name, $fish_number, $min_value, $max_value);
