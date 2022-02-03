469e8ec8d5bc5bf25e8d4b30e89856ea6a46b04f
Mohammad <tajweb.m@gmail.com>
2022-02-03 09:03:11 +0500

    Edited Business logic

--- Validation/order.php
+++ Validation/order.php
@@ -34,8 +34,22 @@ function validateMax($fish_name, $max_value)
     return "Max number of characters are ok! ";
 }
 
-function validateInArray($fish_name)
+function validateInArray($fish_name, $fishes)
 {
+    if (in_array($fish_name, $fishes)) {
+        return "The fish is in list!";
+    }
+    return "The fish is not in list!";
+}
+
+function orderToFisherman($fish_name, $fish_number)
+{
+    $validate_string = validateString($fish_name);
+    $validate_integer = validateInteger($fish_number);
+    $min_value = 3;
+    $max_value = 20;
+    $validate_min = validateMin($fish_name, $min_value);
+    $validate_max = validateMax($fish_name, $max_value);
     $fishes = array(
         'Щука',
         'Судак',
@@ -44,25 +58,32 @@ function validateInArray($fish_name)
         'Толстолобик',
         'Форель'
     );
-    if (in_array($fish_name, $fishes)) {
-        return "The fish is in list!";
-    }
-    return "The fish is not in list!";
-}
-
-function orderToFisherman($fish_name, $fish_number, $min_value, $max_value)
-{
-    $validate_string = validateString($fish_name);
-    $validate_integer = validateInteger($fish_number);
-    $validate_min = validateMin($fish_name, $min_value);
-    $validate_max = validateMax($fish_name, $max_value);
-    $in_array = validateInArray($fish_name);
+    $in_array = validateInArray($fish_name, $fishes);
     return $validate_string . $validate_integer . $validate_min . $validate_max . $in_array;
 }
 
 $fish_name = readline("Input the name of fish, please: ");
 $fish_number = (int)readline("Input the number of fish, please: ");
-$min_value = 3;
-$max_value = 20;
 
-echo orderToFisherman($fish_name, $fish_number, $min_value, $max_value);
+
+echo orderToFisherman($fish_name, $fish_number);
+/*
+function index($request)
+{
+    $fishes = [
+        'Щука',
+        'Судак',
+        'Берш',
+        'Карась',
+        'Толстолобик',
+        'Форель'
+    ];
+    $rules =
+        'required|string|min:3|max:20|in:$fishes';
+    $messages = validate($rules, $requests);
+
+    return count ($messages)
+        ? implode(',/n', $messages)
+        : 'No validation order';
+}
+call_user_func_array("index", ['Судак']);*/
